039fb11ae594fc99e94dc2d0fefaa9f2aa07c76a
brianlu90 <79573586+brianlu90@users.noreply.github.com>
2021-03-28 21:19:42 +0800

    Update main.cpp

--- 7_6_SPI_Master/main.cpp
+++ 7_6_SPI_Master/main.cpp
@@ -1,23 +1,41 @@
 #include "mbed.h"
 
-I2C m_i2c(D14, D15);
-char m_addr = 0x90;
+Thread thread1;
+
+//master
+
+SPI spi(D11, D12, D13); // mosi, miso, sclk
+DigitalOut cs(D9);
+
+void master()
+{
+   int number = 0 ;
+
+   while(1){
+   // Chip must be deselected
+   cs = 1;
+
+   // Setup the spi for 8 bit data, high steady state clock,
+   // second edge capture, with a 1MHz clock rate
+   spi.format(8, 3);
+   spi.frequency(1000000);
+
+   // Select the device by seting chip select low
+   cs = 0;
+
+   printf("send number = %d\n", number);
+
+   int response = spi.write(number);
+   ThisThread::sleep_for(500ms);
+   printf("response from slave = %d\n",response);
+   //cs = 1; // Deselect the device
+   number += 1 ;
+   }
+
+}
+
+
 int main()
 {
-   while (1)
-   {
-      const char tempRegAddr = 0x00;
-
-      m_i2c.write(m_addr, &tempRegAddr, 1);
-      //Set pointer to the temperature register
-
-      char reg[2] = {0, 0};
-      m_i2c.read(m_addr, reg, 2); //Read
-
-      unsigned short res = (reg[0] << 4) | (reg[1] >> 4);
-      float temp =  (float) ((float)res * 0.0625);
-      printf("Temp code=(%d, %d)\r\n", reg[0], reg[1]);
-      printf("Temp = %f.\r\n", temp);
-      ThisThread::sleep_for(1s);
-   }
+   thread1.start(master);
 }
